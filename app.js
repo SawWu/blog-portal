d9ba9b61f60736ba4c208aa6cdf459d1b3f48b7f
SawWu <2804265880@qq.com>
2017-12-30 14:34:24 +0800

    初始化路由

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const swig = require('swig');
 const path = require('path');
 
-
 const app= express();
 
 const port=process.env.PORT || 8081;
@@ -10,6 +9,7 @@ const port=process.env.PORT || 8081;
 swig.setDefaults({
     cache: false
 });
+
 app.set('view cache', false);
 
 app.set('views','./views/');
@@ -18,13 +18,20 @@ app.set('view engine','html');
 
 app.engine('html', swig.renderFile);
 
-
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',function(req,res){
-    res.render('index.html');
-});
+
+app.use('/',require('./routers/main.js'));
+
+app.use('/admin',require('./routers/admin'));
+
+// app.use('/api',require('./routers/api.js'));
+//
+
+
+// app.get('/',function(req,res){
+//     res.render('index.html');
+// });
 
 app.listen(port,function(){
     console.log('blog-protal in run http://127.0.0.1:'+port+'/')
