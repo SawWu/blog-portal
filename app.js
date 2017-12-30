5abe2b38554056a92ff3a46c7aa4adb931917f00
SawWu <2804265880@qq.com>
2017-12-30 18:29:35 +0800

    注册功能

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const swig = require('swig');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app= express();
 
@@ -21,12 +22,15 @@ app.engine('html', swig.renderFile);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 app.use('/',require('./routers/main.js'));
 
-//app.use('/admin',require('./routers/admin'));
+app.use('/admin',require('./routers/admin'));
 
-// app.use('/api',require('./routers/api.js'));
+app.use('/api',require('./routers/api.js'));
 
 
 mongoose.connect('mongodb://localhost:27017/blog', function(err){
