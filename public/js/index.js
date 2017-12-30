a8c0e7756b496c63a9894dab8297c48c3dd86ea6
SawWu <2804265880@qq.com>
2017-12-30 18:01:51 +0800

    初始化页面

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+$(function(){
+    var $loginBox = $('#loginBox');
+    var $registerBox = $('#registerBox');
+    var $userInfo = $('#userInfo');
+
+    //切换到注册面板
+    $loginBox.find('a.colMint').on('click', function() {
+        $registerBox.show();
+        $loginBox.hide();
+    });
+
+    //切换到登录面板
+    $registerBox.find('a.colMint').on('click', function() {
+        $loginBox.show();
+        $registerBox.hide();
+    });
+});
